603f34e186c3a12ac32e9abec86268fcd4268bc4
Wietse Boon <wietseboon@gmail.com>
2024-09-25 16:24:44 +0200

    cleaned up Hodge systems

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import porepy as pp
 import pygeon as pg
 from pygeon.numerics.differentials import exterior_derivative as diff
 from pygeon.numerics.linear_system import create_restriction
+from pygeon.numerics.innerproducts import mass_matrix
+from pygeon.numerics.stiffness import stiff_matrix
 
 
 class Poincare:
@@ -19,7 +21,7 @@ class Poincare:
         """
         self.mdg = mdg
         self.dim = mdg.dim_max()
-        self.flag_entities()
+        self.define_bar_spaces()
         self.create_operators()
 
     def euler_characteristic(self):
@@ -33,14 +35,25 @@ class Poincare:
             case 3:
                 return sd.num_nodes - sd.num_ridges + sd.num_faces - sd.num_cells
 
-    def flag_entities(self):
+    def define_bar_spaces(self):
         """
         Flag the mesh entities that will be used to generate the Poincaré operators
         """
-        self.flagged_cells = np.ones(self.mdg.num_subdomain_cells(), dtype=bool)
-        self.flagged_faces = pg.SpanningTree(mdg, "all_bdry").flagged_faces
-        self.flagged_edges = self.flag_edges()
-        self.flagged_nodes = self.flag_nodes()
+        # Preallocation
+        self.bar_spaces = [None] * (self.dim + 1)
+
+        # Cells
+        self.bar_spaces[self.dim] = np.zeros(self.mdg.num_subdomain_cells(), dtype=bool)
+
+        # Faces
+        self.bar_spaces[self.dim - 1] = pg.SpanningTree(mdg, "all_bdry").flagged_faces
+
+        # Edges in 3D
+        if self.dim == 3:
+            self.bar_spaces[1] = self.flag_edges()
+
+        # Nodes
+        self.bar_spaces[0] = self.flag_nodes()
 
     def flag_edges(self):
         """
@@ -49,36 +62,34 @@ class Poincare:
         Returns:
             np.ndarray: boolean array with flagged edges
         """
-        if self.dim == 2:
-            return ~self.flagged_faces
-        elif self.dim == 3:
-            grad = pg.grad(self.mdg)
-            incidence = grad.T @ grad
 
-            root = self.find_central_node()
+        grad = pg.grad(self.mdg)
+        incidence = grad.T @ grad
 
-            tree = sps.csgraph.breadth_first_tree(incidence, root, directed=False)
-            c_start, c_end, _ = sps.find(tree)
+        root = self.find_central_node()
 
-            rows = np.hstack((c_start, c_end))
-            cols = np.hstack([np.arange(c_start.size)] * 2)
-            vals = np.ones_like(rows)
+        tree = sps.csgraph.breadth_first_tree(incidence, root, directed=False)
+        c_start, c_end, _ = sps.find(tree)
 
-            edge_finder = sps.csc_array(
-                (vals, (rows, cols)), shape=(grad.shape[1], tree.nnz)
-            )
-            edge_finder = np.abs(grad) @ edge_finder
-            I, _, V = sps.find(edge_finder)
-            tree_edges = I[V == 2]
+        rows = np.hstack((c_start, c_end))
+        cols = np.hstack([np.arange(c_start.size)] * 2)
+        vals = np.ones_like(rows)
 
-            flagged_edges = np.zeros(grad.shape[0], dtype=bool)
-            flagged_edges[tree_edges] = True
+        edge_finder = sps.csc_array(
+            (vals, (rows, cols)), shape=(grad.shape[1], tree.nnz)
+        )
+        edge_finder = np.abs(grad) @ edge_finder
+        I, _, V = sps.find(edge_finder)
+        tree_edges = I[V == 2]
 
-            return flagged_edges
+        flagged_edges = np.ones(grad.shape[0], dtype=bool)
+        flagged_edges[tree_edges] = False
+
+        return flagged_edges
 
     def find_central_node(self):
 
-        sd = mdg.subdomains()[0]
+        sd = self.mdg.subdomains()[0]
         center = np.mean(sd.nodes, axis=1, keepdims=True)
         dists = np.linalg.norm(sd.nodes - center, axis=0)
 
@@ -105,30 +116,29 @@ class Poincare:
         n = self.dim
         self.op = [None] * (n + 1)
 
-        # Cells to faces, similar to the spanning tree
+        # Cells to faces, similar to the SpanningTree
         if n > 1:
-            self.op[n] = self.create_op(n, self.flagged_cells, self.flagged_faces)
+            self.op[n] = self.create_op(n)
 
         # Faces to edges (only for 3D)
         if n > 2:
-            self.op[2] = self.create_op(2, ~self.flagged_faces, ~self.flagged_edges)
+            self.op[2] = self.create_op(2)
 
         # Edges to nodes
-        op_nodal = self.create_op(1, self.flagged_edges, self.flagged_nodes)
+
+        op_nodal = self.create_op(1)
         subtract_mean = lambda x: x - np.mean(x)
         self.op[1] = lambda f: subtract_mean(op_nodal(f))
 
         # Nodes to the constants
         self.op[0] = lambda f: np.full_like(f, np.mean(f))
 
-    def create_op(self, k, flag_dom, flag_ran):
+    def create_op(self, k):
         """
         Create the Poincaré operator for k-forms
 
         Args:
             k (int): order of the form
-            flag_dom (np.ndarray): flagged k-entities in the domain
-            flag_ran (np.ndarray): flagged (k-1)-entities in the range
 
         Returns:
             function: the Poincaré operator
@@ -136,12 +146,12 @@ class Poincare:
         n_minus_k = self.dim - k
         _diff = diff(self.mdg, n_minus_k + 1)
 
-        R_dom = create_restriction(flag_dom)
-        R_ran = create_restriction(flag_ran)
+        R_0 = create_restriction(~self.bar_spaces[k])
+        R_bar = create_restriction(self.bar_spaces[k - 1])
 
-        pi_bar_diff = R_dom @ _diff @ R_ran.T
+        pi_0_d_bar = R_0 @ _diff @ R_bar.T
 
-        return lambda f: R_ran.T @ sps.linalg.spsolve(pi_bar_diff, R_dom @ f)
+        return lambda f: R_bar.T @ sps.linalg.spsolve(pi_0_d_bar, R_0 @ f)
 
     def apply(self, k, f):
         """Apply the Poincare operator
@@ -195,165 +205,91 @@ class Poincare:
 
 if __name__ == "__main__":
 
-    N, dim = 5, 3
-    sd = pg.unit_grid(2, 0.15, as_mdg=False)
+    N, dim = 10, 2
+
+    sd = pg.unit_grid(dim, 1 / N, as_mdg=False)
     mdg = pg.as_mdg(sd)
     pg.convert_from_pp(mdg)
     mdg.compute_geometry()
 
-    tree = pg.SpanningTree(mdg, "all_bdry")
-    tree.visualize_2d(mdg, "tree.pdf", False, True, True)
+    # tree = pg.SpanningTree(mdg, "all_bdry")
+    # tree.visualize_2d(mdg, "tree.pdf", False, True, True)
 
     f_0 = np.random.rand(sd.num_nodes)
     f_1 = np.random.rand(sd.num_ridges)
     f_2 = np.random.rand(sd.num_faces)
     f_3 = np.random.rand(sd.num_cells)
 
+    f = [f_0, f_1, f_2, f_3]
+
+    if dim == 2:
+        f = f[1:]
+
     poin = Poincare(mdg)
+    pg.cell_stiff
 
-    for k, f in enumerate([f_0, f_1, f_2, f_3]):
-        pdf, dpf = poin.decompose(k, f)
-        assert np.allclose(f, pdf + dpf)
+    for k, f_ in enumerate(f):
+        pdf, dpf = poin.decompose(k, f_)
+        assert np.allclose(f_, pdf + dpf)
 
-        poin.check_chain_property(k, f)
+        poin.check_chain_property(k, f_)
 
-    # Solve a Hodge Laplace problem
-    S_c = pg.cell_stiff(mdg)
-    S_f = pg.face_stiff(mdg)
-    S_r = pg.ridge_stiff(mdg)
-    S_p = pg.peak_stiff(mdg)
+    # assemble matrices
+    M = [mass_matrix(mdg, dim - k, None) for k in range(dim + 1)]
+    S = [stiff_matrix(mdg, dim - k, None) for k in range(dim + 1)]
+    D = [diff(mdg, dim - k) for k in range(dim)]
+    MD = [M[k + 1] @ D[k] for k in range(dim)]
 
-    # d_c = pg.peak_mass(mdg) @ diff(mdg, 0)
-    d_f = pg.cell_mass(mdg) @ pg.div(mdg)
-    d_r = pg.face_mass(mdg) @ pg.curl(mdg)
-    d_p = pg.ridge_mass(mdg) @ pg.grad(mdg)
+    for k in range(dim, 0, -1):
+        print("k = {}".format(k))
 
-    print("k = n")
+        A = sps.bmat([[M[k - 1], -MD[k - 1].T], [MD[k - 1], S[k]]])
+        LS = pg.LinearSystem(A, np.hstack((M[k - 1] @ f[k - 1], M[k] @ f[k])))
 
-    A = sps.bmat([[pg.face_mass(mdg), -d_f.T], [d_f, S_c]])
-    LS = pg.LinearSystem(
-        A, np.hstack((np.zeros(sd.num_faces), pg.cell_mass(mdg) @ f_3))
-    )
-    vu = LS.solve()
-    print(len(vu))
+        vu = LS.solve()
+        print(len(vu))
 
-    v_true = vu[: S_f.shape[0]]
-    u_true = vu[S_f.shape[0] :]
+        v_true = vu[: M[k - 1].shape[0]]
+        u_true = vu[M[k - 1].shape[0] :]
 
-    # step 1
-    LS = pg.LinearSystem(S_f, d_f.T @ f_3)
-    LS.flag_ess_bc(~poin.flagged_faces, np.zeros_like(poin.flagged_faces))
-    print(np.sum(poin.flagged_faces))
+        # Step 1
+        LS = pg.LinearSystem(S[k - 1], MD[k - 1].T @ f[k])
+        LS.flag_ess_bc(~poin.bar_spaces[k - 1], np.zeros_like(poin.bar_spaces[k - 1]))
+        print(np.sum(poin.bar_spaces[k - 1]))
 
-    v0 = LS.solve()
+        v0 = LS.solve()
 
-    # step 2
-    LS = pg.LinearSystem(S_r, d_r.T @ (f_2 - v0))
-    LS.flag_ess_bc(poin.flagged_edges, np.zeros_like(poin.flagged_edges))
-    print(np.sum(~poin.flagged_edges))
+        # Step 2
+        if k >= 2:
+            LS = pg.LinearSystem(S[k - 2], MD[k - 2].T @ (f[k - 1] - v0))
+            LS.flag_ess_bc(
+                ~poin.bar_spaces[k - 2], np.zeros_like(poin.bar_spaces[k - 2])
+            )
+            print(np.sum(poin.bar_spaces[k - 2]))
 
-    v1 = LS.solve()
+            v1 = LS.solve()
+            v = v0 + D[k - 2] @ v1
+        else:
+            print(0)
+            v = v0 - np.mean(v0 - v_true)
 
-    v = v0 + pg.curl(mdg) @ v1
-    assert np.allclose(v_true, v)
+        assert np.allclose(v_true, v)
 
-    # step 4
-    LS = pg.LinearSystem(S_f, pg.face_mass(mdg) @ (v - f_2))
-    LS.flag_ess_bc(~poin.flagged_faces, np.zeros_like(poin.flagged_faces))
-    print(np.sum(poin.flagged_faces))
+        # Step 3
+        LS = pg.LinearSystem(S[k], M[k] @ f[k] - MD[k - 1] @ v0)
+        LS.flag_ess_bc(~poin.bar_spaces[k], np.zeros_like(poin.bar_spaces[k]))
+        print(np.sum(poin.bar_spaces[k]))
 
-    v4 = LS.solve()
+        v3 = LS.solve()
 
-    u = pg.div(mdg) @ v4
-    assert np.allclose(u_true, u)
+        # Step 4
+        LS = pg.LinearSystem(S[k - 1], M[k - 1] @ (v - f[k - 1]) - MD[k - 1].T @ v3)
+        LS.flag_ess_bc(~poin.bar_spaces[k - 1], np.zeros_like(poin.bar_spaces[k - 1]))
+        print(np.sum(poin.bar_spaces[k - 1]))
 
-    ## vector laplacian (k = n - 1)
-    print("k = n - 1")
+        v4 = LS.solve()
 
-    A = sps.bmat([[pg.ridge_mass(mdg), -d_r.T], [d_r, S_f]])
-    LS = pg.LinearSystem(
-        A, np.hstack((pg.ridge_mass(mdg) @ f_1, pg.face_mass(mdg) @ f_2))
-    )
-    vu = LS.solve()
-    print(len(vu))
-
-    v_true = vu[: S_r.shape[0]]
-    u_true = vu[S_r.shape[0] :]
-
-    # step 1
-    LS = pg.LinearSystem(S_r, d_r.T @ f_2)
-    LS.flag_ess_bc(poin.flagged_edges, np.zeros_like(poin.flagged_edges))
-    print(np.sum(~poin.flagged_edges))
-
-    v0 = LS.solve()
-
-    # step 2
-    LS = pg.LinearSystem(S_p, d_p.T @ (f_1 - v0))
-    LS.flag_ess_bc(~poin.flagged_nodes, np.zeros_like(poin.flagged_nodes))
-    print(np.sum(poin.flagged_nodes))
-
-    v1 = LS.solve()
-
-    v = v0 + pg.grad(mdg) @ v1
-    assert np.allclose(v_true, v)
-
-    # step 3
-    LS = pg.LinearSystem(S_f, pg.face_mass(mdg) @ f_2 - d_r @ v0)
-    LS.flag_ess_bc(~poin.flagged_faces, np.zeros_like(poin.flagged_faces))
-    print(np.sum(poin.flagged_faces))
-
-    v3 = LS.solve()
-
-    # step 4
-    LS = pg.LinearSystem(S_r, pg.ridge_mass(mdg) @ (v - f_1) - d_r.T @ v3)
-    LS.flag_ess_bc(poin.flagged_edges, np.zeros_like(poin.flagged_edges))
-    print(np.sum(~poin.flagged_edges))
-
-    v4 = LS.solve()
-
-    u = v3 + pg.curl(mdg) @ v4
-    assert np.allclose(u_true, u)
-
-    ## vector laplacian (k = n - 2)
-    print("k = n - 2")
-
-    A = sps.bmat([[pg.peak_mass(mdg), -d_p.T], [d_p, S_r]])
-    LS = pg.LinearSystem(
-        A, np.hstack((pg.peak_mass(mdg) @ f_0, pg.ridge_mass(mdg) @ f_1))
-    )
-    vu = LS.solve()
-    print(len(vu))
-
-    v_true = vu[: S_p.shape[0]]
-    u_true = vu[S_p.shape[0] :]
-
-    # step 1
-    LS = pg.LinearSystem(S_p, d_p.T @ f_1)
-    LS.flag_ess_bc(~poin.flagged_nodes, np.zeros_like(poin.flagged_nodes))
-    print(np.sum(poin.flagged_nodes))
-
-    v0 = LS.solve()
-
-    # step 2
-
-    v = v0 - np.mean(v0) + np.mean(v_true)
-    assert np.allclose(v_true, v)
-
-    # step 3
-    LS = pg.LinearSystem(S_r, pg.ridge_mass(mdg) @ f_1 - d_p @ v0)
-    LS.flag_ess_bc(poin.flagged_edges, np.zeros_like(poin.flagged_edges))
-    print(np.sum(~poin.flagged_edges))
-
-    v3 = LS.solve()
-
-    # step 4
-    LS = pg.LinearSystem(S_p, pg.peak_mass(mdg) @ (v - f_0) - d_p.T @ v3)
-    LS.flag_ess_bc(~poin.flagged_nodes, np.zeros_like(poin.flagged_nodes))
-    print(np.sum(poin.flagged_nodes))
-
-    v4 = LS.solve()
-
-    u = v3 + pg.grad(mdg) @ v4
-    assert np.allclose(u_true, u)
+        u = v3 + D[k - 1] @ v4
+        assert np.allclose(u_true, u)
 
     pass
